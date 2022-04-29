2c7479f9fe6ff13ee7553ec097dec5f2d0054bce
Marcelo Vieira <48066643+cecelo01@users.noreply.github.com>
2022-04-29 11:14:43 -0300

    Update

---
+++ src/br/systemtime/SystemTime.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.systemtime;
+
+import java.util.Date;
+
+
+/**
+ *
+ * @author Marce
+ */
+public class SystemTime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Date date = new Date();
+        System.out.println("A hora certa é: " + date);
+        //System.out.println(date.toString());
+    }
+
+}
